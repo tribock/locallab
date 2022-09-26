7a99aea61065b54d48e547b9953fbd5c0952025b
Roger Zimmermann <roger.zimmermann@inventx.ch>
2022-09-26 08:58:41 +0200

    decreasd memory

--- clusters/ocp-dev.sh
+++ clusters/ocp-dev.sh
@@ -3,4 +3,4 @@
 ISOIMAGE=discovery_image_itix-dev-local.iso
 ISOPATH=/var/lib/libvirt/images/
 sudo cp ~/Downloads/$ISOIMAGE $ISOPATH/
-sudo virt-install -n ocp-dev --memory 49152 --vcpus=8 --os-variant=fedora-coreos-stable --accelerate -v --cpu host-passthrough,cache.mode=passthrough --disk path=$ISOPATH/ocp-dev.qcow2,size=120 --network network=ocp-dev,mac=02:01:00:00:00:66 --cdrom $ISOPATH/$ISOIMAGE
+sudo virt-install -n ocp-dev --memory 32768 --vcpus=8 --os-variant=fedora-coreos-stable --accelerate -v --cpu host-passthrough,cache.mode=passthrough --disk path=$ISOPATH/ocp-dev.qcow2,size=120 --network network=ocp-dev,mac=02:01:00:00:00:66 --cdrom $ISOPATH/$ISOIMAGE
